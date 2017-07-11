64dc53096be504ca6b8aa81b9ff1f80be08b4009
Esben Andreasen <esbenandreasen@gmail.com>
2017-07-11 15:33:41 +0200

    generalize DescribedProperties: single map for the meta-properties

--- src/dk/brics/inspector/api/model/values/DescribedProperties.java
+++ src/dk/brics/inspector/api/model/values/DescribedProperties.java
@@ -4,25 +4,16 @@ import java.util.Map;
 
 /**
  * Description of the properties of object in the analysis.
- * TODO make less TAJS/JavaScript-specific structure.
  */
 public class DescribedProperties {
 
-    public final CompositeValue prototype;
-
-    public final CompositeValue internal;
-
-    public final CompositeValue array;
-
-    public final CompositeValue nonArray;
+    public final Map<String, CompositeValue> metaProperties;
 
     public final Map<String, CompositeValue> properties;
 
-    public DescribedProperties(CompositeValue prototype, CompositeValue internal, CompositeValue array, CompositeValue nonArray, Map<String, CompositeValue> properties) {
-        this.prototype = prototype;
-        this.internal = internal;
-        this.array = array;
-        this.nonArray = nonArray;
+    public DescribedProperties(Map<String, CompositeValue> metaProperties, Map<String, CompositeValue> properties) {
+        this.metaProperties = metaProperties;
+
         this.properties = properties;
     }
 
@@ -33,19 +24,14 @@ public class DescribedProperties {
 
         DescribedProperties that = (DescribedProperties) o;
 
-        if (prototype != null ? !prototype.equals(that.prototype) : that.prototype != null) return false;
-        if (internal != null ? !internal.equals(that.internal) : that.internal != null) return false;
-        if (array != null ? !array.equals(that.array) : that.array != null) return false;
-        if (nonArray != null ? !nonArray.equals(that.nonArray) : that.nonArray != null) return false;
+        if (metaProperties != null ? !metaProperties.equals(that.metaProperties) : that.metaProperties != null)
+            return false;
         return properties != null ? properties.equals(that.properties) : that.properties == null;
     }
 
     @Override
     public int hashCode() {
-        int result = prototype != null ? prototype.hashCode() : 0;
-        result = 31 * result + (internal != null ? internal.hashCode() : 0);
-        result = 31 * result + (array != null ? array.hashCode() : 0);
-        result = 31 * result + (nonArray != null ? nonArray.hashCode() : 0);
+        int result = metaProperties != null ? metaProperties.hashCode() : 0;
         result = 31 * result + (properties != null ? properties.hashCode() : 0);
         return result;
     }
